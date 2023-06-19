dedf84e7240f68328f45a894838b4ac2d05f372c
Alex C <alex.coaton@devacademy.co.nz>
2023-06-19 12:38:22 +1200

    Added env config for dokku paths.

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,19 @@
 import { join } from 'node:path'
 import express from 'express'
 import widgets from './routes/widgets'
+import * as Path from 'node:path'
 
 const server = express()
 server.use(express.json())
 server.use(express.static(join(__dirname, 'public')))
 
 server.use('/api/v1/widgets', widgets)
+
+if (process.env.NODE_ENV === 'production') {
+  server.use('/assets', express.static(Path.resolve(__dirname, '../assets')))
+  server.get('*', (req, res) => {
+    res.sendFile(Path.resolve(__dirname, '../index.html'))
+  })
+}
 
 export default server
