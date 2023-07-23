acc13d8ff3a9f3318964d71567028b7ce506f718
maia-tarapipipi <tarapipipim@gmail.com>
2023-07-24 11:14:28 +1200

    re-ran seeds and npm i

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import server from './server'
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3002
 
 server.listen(port, () => {
   // eslint-disable-next-line no-console
